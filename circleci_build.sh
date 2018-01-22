43512c5f33e4ae56d2d3e4cb2cfd8db4aa7fc1ee
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-01-22 11:45:51 +0900

    build

---
+++ circleci_build.sh
@@ -0,0 +1,31 @@
+# for centos 6,7
+
+echo platform name: $1
+echo SHA1: ${CIRCLE_SHA1}
+
+ARCH=$1
+
+uname -a
+
+if [[ "$1" == "macos" ]]; then
+    MACVER=`sw_vers -productVersion`
+    ARCH=${1}_${MACVER}
+    echo ${MACVER}   
+else
+
+    cat /etc/redhat-release
+    yum install -y -q gcc gcc-c++
+    yum install -y -q epel-release
+    #    yum install -y -q cmake3
+    yum install -y -q make
+    ln -s /usr/bin/cmake3 /usr/bin/cmake
+
+fi    
+
+
+sh autogen.sh
+./configure
+make
+
+mkdir /artifacts
+cp .libs/libuv.a /artifacts/libuv_${ARCH}_${CIRCLE_SHA1}.a
