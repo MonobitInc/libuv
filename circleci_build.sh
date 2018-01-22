78fc0ead3b3809a0322da18cfe879be6b50684cc
Kengo Nakajima <kengo.nakajima@gmail.com>
2018-01-22 17:07:55 +0900

    packaging

--- circleci_build.sh
+++ circleci_build.sh
@@ -19,8 +19,7 @@ else
     #    yum install -y -q cmake3
     yum install -y -q libtool
     yum install -y -q make
-    ln -s /usr/bin/cmake3 /usr/bin/cmake
-
+    #    ln -s /usr/bin/cmake3 /usr/bin/cmake
 fi    
 
 
@@ -29,4 +28,7 @@ sh autogen.sh
 make
 
 mkdir /artifacts
+cp -r include include_${ARCH}_${CIRCLE_SHA1}
+zip -r include_${ARCH}_${CIRCLE_SHA1}.zip include_${ARCH}_${CIRCLE_SHA1}
+cp include_${ARCH}_${CIRCLE_SHA1}.zip /artifacts/
 cp .libs/libuv.a /artifacts/libuv_${ARCH}_${CIRCLE_SHA1}.a
