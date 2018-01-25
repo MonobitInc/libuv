b4fd334cda04ba57b1b675bcf9988816bb53117f
kengo nakajima <kengo.nakajima@gmail.com>
2018-01-25 09:40:23 +0900

    s3 put test

--- circleci_build.sh
+++ circleci_build.sh
@@ -19,6 +19,22 @@ else
     yum install -y -q libtool
     yum install -y -q make
     yum install -y -q zip
+
+    # aws config
+    curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+    sudo python get-pip.py
+    sudo pip install awscli
+
+    AWSDIR=~/.aws
+    mkdir -p 
+    
+    echo "[default]" > ${AWSDIR}/config
+    echo "output = json" >> ${AWSDIR}/config
+    echo "region = ap-northeast-1" >> ${AWSDIR}/config
+
+    echo "[default]" > ${AWSDIR}/credentials
+    echo "aws_access_key_id=${AWS_ACCESS_KEY_ID}" >> ${AWSDIR}/credentials
+    echo "aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" >> ${AWSDIR}/credentials
 fi    
 
 
@@ -36,6 +52,6 @@ cp .libs/libuv.a /artifacts/${BASENAME}/
 
 cd /artifacts
 zip -r ${BASENAME}.zip ${BASENAME}
-rm -r ${BASENAME}
 
+aws s3 put s3://appveyor-tmp/libuv_bin/${BASENAME}.zip
 
