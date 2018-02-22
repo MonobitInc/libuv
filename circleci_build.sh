6a9a4d57f72d78be57b8517a950e6fa454174f81
kengo nakajima <kengo.nakajima@gmail.com>
2018-02-22 16:48:46 +0900

    bash

--- circleci_build.sh
+++ circleci_build.sh
@@ -23,7 +23,7 @@ else
     # aws config
     yum install -y -q git
     git submodule update --init build_tools
-    build_tools/setup_awscli_linux.sh ${ARCH}
+    bash -e -x build_tools/setup_awscli_linux.sh ${ARCH}
 fi    
 
 
