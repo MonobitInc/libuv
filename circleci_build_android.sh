3c07b1f00f1d3e9b7724fdc5e345fd4c57d1cfac
kengo nakajima <kengo.nakajima@gmail.com>
2018-02-28 15:41:53 +0900

    r12b

--- circleci_build_android.sh
+++ circleci_build_android.sh
@@ -15,36 +15,50 @@ yum install -y -q git
 git submodule update --init build_tools
 bash -e -x build_tools/setup_awscli_linux.sh centos7
 
-NDKVER=android-ndk-r10e
-NDKBIN=${NDKVER}-linux-x86_64.bin
-wget --quiet http://dl.google.com/android/ndk/${NDKBIN}
-file ${NDKBIN}
-chmod a+x ${NDKBIN}
-./${NDKBIN} > /dev/null
-ls
-
-NDK_ROOT=`pwd`/${NDKVER}
-NDK_BUILD=${NDK_ROOT}/ndk-build
-
-${NDK_BUILD} NDK_APPLICATION_MK=./jni/Application.mk NDK_PROJECT_PATH=. NDK_OUT=. APP_BUILD_SCRIPT=./jni/Android.mk
-
-# output is in "local" dir
-
+# set up artifacts dir
 BASENAME=libuv_android_${CIRCLE_SHA1}
-
 mkdir /artifacts
 mkdir /artifacts/${BASENAME}
-
-
 cp -r include /artifacts/${BASENAME}/
-mkdir /artifacts/${BASENAME}/mips
-cp local/mips/libuv.a /artifacts/${BASENAME}/mips/
-mkdir /artifacts/${BASENAME}/armeabi-v7a
-cp local/armeabi-v7a/libuv.a /artifacts/${BASENAME}/armeabi-v7a/
-mkdir /artifacts/${BASENAME}/x86
-cp local/x86/libuv.a /artifacts/${BASENAME}/x86/
-mkdir /artifacts/${BASENAME}/armeabi
-cp local/armeabi/libuv.a /artifacts/${BASENAME}/armeabi/
+
+
+function setup_build_ndk() {
+    NDKVER=$1
+    NDKBIN=${NDKVER}-linux-x86_64.bin
+    wget --quiet http://dl.google.com/android/ndk/${NDKBIN}
+    file ${NDKBIN}
+    chmod a+x ${NDKBIN}
+    ./${NDKBIN} > /dev/null
+    ls
+
+    NDK_ROOT=`pwd`/${NDKVER}
+    NDK_BUILD=${NDK_ROOT}/ndk-build
+    ${NDK_BUILD} NDK_APPLICATION_MK=./jni/Application.mk NDK_PROJECT_PATH=. NDK_OUT=. APP_BUILD_SCRIPT=./jni/Android.mk
+
+    # output is in "local" dir
+    OUTDIR=/artifacts/${BASENAME}/${NDKVER}
+    mkdir ${OUTDIR}
+    mkdir ${OUTDIR}/mips    
+    mkdir ${OUTDIR}/armeabi-v7a
+    mkdir ${OUTDIR}/x86
+    mkdir ${OUTDIR}/armeabi
+
+    cp local/mips/libuv.a ${OUTDIR}/mips/
+    cp local/armeabi-v7a/libuv.a ${OUTDIR}/armeabi-v7a/
+    cp local/x86/libuv.a ${OUTDIR}/x86/
+    cp local/armeabi/libuv.a ${OUTDIR}/armeabi/
+    
+}
+
+
+# build
+NDK10VER=android-ndk-r10e
+NDK12VER=android-ndk-r12b
+
+setup_build_ndk(NDK10VER)
+setup_build_ndk(NDK12VER)
+
+
 
 cd /artifacts
 zip -r ${BASENAME}.zip ${BASENAME}
