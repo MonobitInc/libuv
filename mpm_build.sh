f634b73b1aa3054ba2456fa39ebcbaa8ce7e8b0b
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-04-03 17:13:32 +0900

    mpm

---
+++ mpm_build.sh
@@ -0,0 +1,30 @@
+set -x
+
+UNAME=`uname -sm | sed -e s,x86_64,i386,`
+echo $UNAME
+DATE=`date "+%Y%m%d%H%M"`
+
+
+if [ "$UNAME" = "Darwin i386" ]; then
+    OSXVER=`sw_vers -productVersion | ruby -e 'print gets.split(".")[0..1].join(".")'`
+    PKGDIR="libuv-macos$OSXVER-$DATE"
+else
+    PKGDIR="udp-el7-$DATE"
+fi
+
+sh autogen.sh
+./configure
+make CFLAGS=-fPIC
+
+mkdir /tmp/${PKGDIR}
+
+cp -r include /tmp/${PKGDIR}/
+cp .libs/libuv.a /tmp/${PKGDIR}/
+git log | head -1 | awk '{print $2}' > /tmp/${PKGDIR}/git_revision.txt
+
+cd /tmp
+zip -r ${PKGDIR}.zip ${PKGDIR}
+
+aws s3 cp ${PKGDIR}.zip s3://monobit/libuv/${PKGDIR}.zip
+
+
