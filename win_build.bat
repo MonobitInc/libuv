d80bdc381a68fedc86daaa942dfd977e5a719983
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-06-21 08:32:00 +0000

    rename

--- win_build.bat
+++ win_build.bat
@@ -4,23 +4,22 @@ echo on
 
 setlocal
 
-rem         $env:VCVARSALL_BAT="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
-set GYP_MSVS_VERSION=2017
+cmake .
+
 rd /s /q Debug Release x86 x64 x86.dbg x64.dbg
-rem call vcbuild.bat release x64 static vs2017
-rem if %errorlevel% neq 0 (  exit /B )
-rem rename Release x64
-rem call vcbuild.bat release x86 static vs2017
-rem if %errorlevel% neq 0 (  exit /B )
-rem rename Release x86
-rem call vcbuild.bat debug x64 static vs2017
-rem if %errorlevel% neq 0 (  exit /B )
-rem rename Debug x64.dbg
+call vcbuild.bat release x64 static vs2017
+if %errorlevel% neq 0 (  exit /B )
+rename Release x64
+call vcbuild.bat release x86 static vs2017
+if %errorlevel% neq 0 (  exit /B )
+rename Release x86
+call vcbuild.bat debug x64 static vs2017
+if %errorlevel% neq 0 (  exit /B )
+rename Debug x64.dbg
 call vcbuild.bat debug x86 static vs2017
 if %errorlevel% neq 0 (  exit /B )
 rename Debug x86.dbg
 
-exit /B
 
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c%%a%%b)
 For /f "tokens=1-2 delims=/: " %%a in ("%TIME%") do (if %%a LSS 10 (set mytime=0%%a%%b) else (set mytime=%%a%%b)) 
@@ -35,7 +34,12 @@ mkdir %zipdir%\x86
 mkdir %zipdir%\x64
 xcopy /E include %zipdir%\include
 copy x86\lib\libuv.lib %zipdir%\x86\libuv.lib
+copy x86.dbg\lib\libuv.lib %zipdir%\x86\libuv_debug.lib
+copy x86.dbg\libuv.pdb %zipdir%\x86\libuv.pdb
 copy x64\lib\libuv.lib %zipdir%\x64\libuv.lib
+copy x64.dbg\lib\libuv.lib %zipdir%\x64\libuv_debug.lib
+copy x64.dbg\libuv.pdb %zipdir%\x64\libuv.pdb
+
 git log -1 --pretty=oneline > %zipir%\git_revision.txt
 dir %zipdir%
 rd /s /q %basename%-latest
