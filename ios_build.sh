d1a458f75eb5f08b5578f5fd040c6f51baf51334
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-08-09 16:45:24 +0900

    bash

--- ios_build.sh
+++ ios_build.sh
@@ -3,7 +3,7 @@
 
 IOSVER=12.4
 
-SDKOPT=-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${IOSVER}.sdk -arch arm64 -arch armv7 -arch armv7s -miphoneos-version-min=12.0  
+SDKOPT="-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${IOSVER}.sdk -arch arm64 -miphoneos-version-min=12.0"
 
 SRCS="src/fs-poll.c src/inet.c src/threadpool.c src/uv-common.c src/version.c src/unix/async.c src/unix/core.c src/unix/dl.c src/unix/fs.c src/unix/getaddrinfo.c src/unix/getnameinfo.c src/unix/loop.c src/unix/loop-watcher.c src/unix/pipe.c src/unix/poll.c src/unix/process.c src/unix/signal.c src/unix/stream.c src/unix/tcp.c src/unix/thread.c src/timer.c src/unix/tty.c src/unix/udp.c src/unix/proctitle.c src/unix/pthread-fixes.c"
 
