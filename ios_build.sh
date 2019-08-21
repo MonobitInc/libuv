bc0733e25f4602b29b4d9e5a64375808c4dc02f1
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-08-21 12:18:03 +0900

    linking

--- ios_build.sh
+++ ios_build.sh
@@ -1,15 +1,17 @@
 # build libuv for ios 12.4
 
+set -xeu
+
 rm libuv.a *.o
 
 
 IOSVER=12.4
 
-SDKOPT="-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${IOSVER}.sdk -arch arm64 -miphoneos-version-min=12.0"
+SDKOPT="-isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS${IOSVER}.sdk -arch arm64 -miphoneos-version-min=12.0 -fembed-bitcode"
 
-SRCS="src/fs-poll.c src/inet.c src/threadpool.c src/uv-common.c src/version.c src/unix/async.c src/unix/core.c src/unix/dl.c src/unix/fs.c src/unix/getaddrinfo.c src/unix/getnameinfo.c src/unix/loop.c src/unix/loop-watcher.c src/unix/pipe.c src/unix/poll.c src/unix/process.c src/unix/signal.c src/unix/stream.c src/unix/tcp.c src/unix/thread.c src/timer.c src/unix/tty.c src/unix/udp.c src/unix/proctitle.c src/unix/pthread-fixes.c"
+SRCS="src/fs-poll.c src/inet.c src/threadpool.c src/uv-common.c src/version.c src/unix/async.c src/unix/core.c src/unix/dl.c src/unix/fs.c src/unix/getaddrinfo.c src/unix/getnameinfo.c src/unix/loop.c src/unix/loop-watcher.c src/unix/pipe.c src/unix/poll.c src/unix/process.c src/unix/signal.c src/unix/stream.c src/unix/tcp.c src/unix/thread.c src/timer.c src/unix/tty.c src/unix/udp.c src/unix/proctitle.c src/unix/pthread-fixes.c src/strscpy.c src/unix/darwin.c src/unix/kqueue.c src/unix/fsevents.c"
 
-OBJS="fs-poll.o inet.o threadpool.o uv-common.o version.o async.o core.o dl.o fs.o getaddrinfo.o getnameinfo.o loop.o loop-watcher.o pipe.o poll.o process.o signal.o stream.o tcp.o thread.o timer.o tty.o udp.o proctitle.o pthread-fixes.o"
+OBJS="fs-poll.o inet.o threadpool.o uv-common.o version.o async.o core.o dl.o fs.o getaddrinfo.o getnameinfo.o loop.o loop-watcher.o pipe.o poll.o process.o signal.o stream.o tcp.o thread.o timer.o tty.o udp.o proctitle.o pthread-fixes.o darwin.o strscpy.o kqueue.o fsevents.o"
 
 clang ${SDKOPT} -c -I./include -I./src ${SRCS}
 
