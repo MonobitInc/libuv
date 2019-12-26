9d0aa6fc6fc23c0ceeb4ed0c620d64a3c1df9c33
Ubuntu <ubuntu@ip-172-31-6-234.ap-northeast-1.compute.internal>
2019-12-26 06:41:41 +0000

    ubuntudetect

--- mpm_build.sh
+++ mpm_build.sh
@@ -1,30 +1,49 @@
 set -x
 
-UNAME=`uname -sm | sed -e s,x86_64,i386,`
-echo $UNAME
-DATE=`date "+%Y%m%d%H%M"`
+UNAME=`uname`
+
+DATESTR=`date "+%Y%m%d%H%M"`
 
 
-if [ "$UNAME" = "Darwin i386" ]; then
-    OSXVER=`sw_vers -productVersion`
-    PKGDIR="libuv-macos$OSXVER-$DATE"
-else
-    PKGDIR="libuv-el7-$DATE"
+if [ "$UNAME" = "Darwin" ]; then
+    echo Darwin detected    
+    OSXVER=`sw_vers -productVersion` # ex. 10.15
+    BASENAME=libuv-macos${OSXVER}-${DATESTR}
 fi
+
+if [[ "${UNAME}" == "Linux" ]]; then
+  EL7=`grep -o "release 7" /etc/redhat-release`
+  if [[ "${EL7}" == "release 7" ]]; then
+    echo "el7 detected"
+    BASENAME=libuv-el7-${DATESTR}
+  else
+    echo "el7 not detected, next, search for ubuntu"    
+  fi
+  UBU=`lsb_release -i -s`
+  if [[ "${UBU}" == "Ubuntu" ]]; then
+      UBUVER=`lsb_release -r -s`
+      BASENAME=libuv-ubuntu${UBUVER}-${DATESTR}
+  else
+      echo "ubuntu not detected"
+      exit 1
+  fi  
+  SYMLINKOPT=-L
+fi
+
 
 sh autogen.sh
 ./configure
 make CFLAGS=-fPIC
 
-mkdir /tmp/${PKGDIR}
+mkdir /tmp/${BASENAME}
 
-cp -r include /tmp/${PKGDIR}/
-cp .libs/libuv.a /tmp/${PKGDIR}/
-git log | head -1 | awk '{print $2}' > /tmp/${PKGDIR}/git_revision.txt
+cp -r include /tmp/${BASENAME}/
+cp .libs/libuv.a /tmp/${BASENAME}/
+git log | head -1 | awk '{print $2}' > /tmp/${BASENAME}/git_revision.txt
 
 cd /tmp
-zip -r ${PKGDIR}.zip ${PKGDIR}
+zip -r ${BASENAME}.zip ${BASENAME}
 
-aws s3 cp ${PKGDIR}.zip s3://monobit/libuv/${PKGDIR}.zip
+aws s3 cp ${BASENAME}.zip s3://monobit/libuv/${BASENAME}.zip
 
 
